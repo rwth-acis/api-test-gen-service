96c88681d5f8d287f701bde2066b5a29a5b42557
Philipp Dolif <git.phil.cd@gmail.com>
2022-07-06 15:45:01 +0200

    Refactor generators

---
+++ api_test_gen_service/src/main/java/i5/las2peer/services/apiTestGenService/generator/GenerationHelper.java
@@ -0,0 +1,115 @@
+package i5.las2peer.services.apiTestGenService.generator;
+
+import i5.las2peer.apiTestModel.RequestAssertion;
+import i5.las2peer.apiTestModel.TestCase;
+import i5.las2peer.apiTestModel.TestRequest;
+import io.swagger.models.Model;
+import io.swagger.models.Operation;
+import io.swagger.models.Swagger;
+import io.swagger.models.parameters.BodyParameter;
+import io.swagger.models.parameters.Parameter;
+import org.json.simple.JSONObject;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class GenerationHelper {
+
+    /**
+     * If the operation contains path parameters, sets them to "" in the test request.
+     *
+     * @param request   TestRequest
+     * @param operation Operation from Swagger documentation.
+     */
+    public static void setEmptyPathParameters(TestRequest request, Operation operation) {
+        JSONObject pathParams = request.getPathParams();
+        operation.getParameters().stream().forEach(parameter -> {
+            if (parameter.getIn().equals("path")) {
+                pathParams.put(parameter.getName(), "");
+            }
+        });
+    }
+
+    /**
+     * Creates a test case with the given name that only contains the given request.
+     *
+     * @param name    Name of the test case.
+     * @param request Request that should be included in the test case.
+     * @return TestCase object.
+     */
+    public static TestCase createTestCase(String name, TestRequest request) {
+        return new TestCase(name, Arrays.asList(request));
+    }
+
+    /**
+     * Creates a test request with given type and path that only contains the given assertion.
+     *
+     * @param type      Type, e.g., GET.
+     * @param path      Request path.
+     * @param assertion Assertion that should be included.
+     * @return TestRequest object.
+     */
+    public static TestRequest createTestRequest(String type, String path, RequestAssertion assertion) {
+        return new TestRequest(type, path, Arrays.asList(assertion));
+    }
+
+    public static TestRequest createTestRequest(String type, String path, int authSelectedAgent, RequestAssertion assertion) {
+        return new TestRequest(type, path, new JSONObject(), authSelectedAgent, "", Arrays.asList(assertion));
+    }
+
+    /**
+     * Creates a test request with given type, path and body that only contains the given assertion.
+     *
+     * @param type      Type, e.g., GET.
+     * @param path      Request path.
+     * @param body      Request body.
+     * @param assertion Assertion that should be included.
+     * @return TestRequest object.
+     */
+    public static TestRequest createTestRequest(String type, String path, String body, RequestAssertion assertion) {
+        return new TestRequest(type, path, new JSONObject(), -1, body, Arrays.asList(assertion));
+    }
+
+    public static void addTestCaseIfNotNull(Map<TestCase, String> testCases, Map.Entry<TestCase, String> testCase) {
+        if (testCase != null) testCases.put(testCase.getKey(), testCase.getValue());
+    }
+
+    public static boolean operationHasBodyParameter(Operation operation) {
+        Parameter parameter = operation.getParameters().stream().filter(param -> param.getIn().equals("body")).findFirst().orElse(null);
+        return parameter != null;
+    }
+
+    public static BodyParameter getOperationBodyParameter(Operation operation) {
+        Parameter parameter = operation.getParameters().stream().filter(param -> param.getIn().equals("body")).findFirst().orElse(null);
+        return (BodyParameter) parameter;
+    }
+
+    public static Model getBodyParameterSchema(Swagger swagger, BodyParameter bodyParameter) {
+        for (Map.Entry<String, Model> entry : swagger.getDefinitions().entrySet()) {
+            String schemaName = entry.getKey();
+            if (getBodyParameterSchemaName(bodyParameter).equals(schemaName)) {
+                Model model = entry.getValue();
+                return model;
+            }
+        }
+        return null;
+    }
+
+    public static String getBodyParameterSchemaName(BodyParameter bodyParameter) {
+        String ref = bodyParameter.getSchema().getReference();
+        return ref.split("#/definitions/")[1];
+    }
+
+    public static List<Parameter> getOperationPathParams(Operation operation) {
+        return operation.getParameters().stream().filter(param -> param.getIn().equals("path")).toList();
+    }
+
+    public static boolean hasNoParameters(Operation operation) {
+        return operation.getParameters().size() == 0;
+    }
+
+    public static boolean statusCodeAllowed(Operation operation, int statusCode) {
+        return operation.getResponses().keySet().contains(String.valueOf(statusCode));
+    }
+}
---
+++ api_test_gen_service/src/main/java/i5/las2peer/services/apiTestGenService/generator/ResourceNotFoundTestGenerator.java
@@ -0,0 +1,46 @@
+package i5.las2peer.services.apiTestGenService.generator;
+
+import i5.las2peer.apiTestModel.StatusCodeAssertion;
+import i5.las2peer.apiTestModel.TestCase;
+import i5.las2peer.apiTestModel.TestRequest;
+import io.swagger.models.HttpMethod;
+import io.swagger.models.Operation;
+import io.swagger.models.Swagger;
+import io.swagger.models.parameters.Parameter;
+import org.json.simple.JSONObject;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static i5.las2peer.services.apiTestGenService.generator.GenerationHelper.*;
+
+public class ResourceNotFoundTestGenerator implements TestCaseGenerator {
+
+    /**
+     * If the operation requires path parameters, this method generates a test case that should use path parameters
+     * for which no resource can be found and asserts on status code 404.
+     *
+     * @param method    HttpMethod
+     * @param operation Operation for which a test case should be generated.
+     * @param path      Path
+     * @return Map entry with TestCase object as key and description as value if test could be generated, null otherwise.
+     */
+    @Override
+    public Map.Entry<TestCase, String> generateTestCase(Swagger swagger, HttpMethod method, Operation operation, String path) {
+        List<Parameter> pathParams = getOperationPathParams(operation);
+        // check if there are path params and 404 response is possible
+        if (pathParams.size() > 0 && statusCodeAllowed(operation, 404)) {
+            // create test that uses path parameter value for which no resource can be found
+            StatusCodeAssertion assertion = new StatusCodeAssertion(0, 404);
+            TestRequest request = new TestRequest(method.name(), path, new JSONObject(), -1, "", Arrays.asList(assertion));
+            setEmptyPathParameters(request, operation);
+            TestCase generatedTestCase = createTestCase(method.name() + " " + path + " not found test", request);
+
+            String description = "The method " + method.name() + " " + path +
+                    " requires a path parameter. Choosing a value for it, so that no resource can be found, should lead to status code 404 (Not found).";
+            return Map.entry(generatedTestCase, description);
+        }
+        return null;
+    }
+}
